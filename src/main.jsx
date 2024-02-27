cce1be00f5d2aad976f1b1c3d884837fc5f41792
Chantal Santos <chantalsantos7@gmail.com>
2024-02-27 14:49:19 +0000

    CHORE: Add bootstrap-icons for bookmarks

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './css/style.css';
 import App from "./App.jsx";
 
